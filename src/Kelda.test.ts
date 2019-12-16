893f9bd6c3fe6bc58f2c24eac04b6d26156d6307
Rufus Raghunath <rraghun@thoughtworks.com>
2019-12-16 16:30:58 +0100

    Pass existing tests using new ThreadPool and Thread abstractions

--- src/Kelda.test.ts
+++ src/Kelda.test.ts
@@ -1,32 +1,71 @@
 import Kelda from "./Kelda";
+import MockWorker from "./dom-mocks/MockWorker";
+import MockURL from "./dom-mocks/MockURL";
+import MockBlob from "./dom-mocks/MockBlob";
 
 describe("Kelda", () => {
+  const work = () => 1 + 1;
+  const timedWork = () =>
+    new Promise(resolve => setTimeout(() => resolve(2), 1000));
+
   it("should throw if threadPoolDepth is 0", () => {
-    expect(() => new Kelda(0)).toThrowError(
-      "Error constructing Kelda: threadPoolDepth must be greater than 0"
+    expect(() => new Kelda({ threadPoolDepth: 0 })).toThrowError(
+      "KeldaError: threadPoolDepth must be greater than 0"
     );
   });
 
   it("should throw if threadPoolDepth is <0", () => {
-    expect(() => new Kelda(-1)).toThrowError(
-      "Error constructing Kelda: threadPoolDepth must be greater than 0"
+    expect(() => new Kelda({ threadPoolDepth: -1 })).toThrowError(
+      "KeldaError: threadPoolDepth must be greater than 0"
     );
   });
 
-  it("should take orders for synchronous work", async () => {
-    const work = () => 1 + 1;
-    const kelda = new Kelda();
-    const result = await kelda.orderWork(work);
+  describe("Running MainThreadJobs", () => {
+    it("should take orders for synchronous work", async () => {
+      const kelda = new Kelda();
+      const result = await kelda.orderWork(work);
 
-    expect(result).toBe(2);
+      expect(result).toBe(2);
+    });
+
+    it("should take orders for asynchronous work", async () => {
+      const kelda = new Kelda();
+      const result = await kelda.orderWork(timedWork);
+
+      expect(result).toBe(2);
+    });
+
+    xit("should do something with the queue?", () => {
+      // what exactly should happen with mainthreadjobs?
+    });
   });
 
-  it("should take orders for asynchronous work", async () => {
-    const work = () =>
-      new Promise(resolve => setTimeout(() => resolve(2), 100));
-    const kelda = new Kelda();
-    const result = await kelda.orderWork(work);
+  // TODO: tests for default options
 
-    expect(result).toBe(2);
+  describe("Running WorkerJobs", () => {
+    // TODO: extract all of this stuff
+    window.Worker = MockWorker as any;
+    window.URL = MockURL as any;
+    window.Blob = MockBlob as any;
+
+    beforeEach(() => {
+      jest.useFakeTimers();
+    });
+
+    afterEach(() => {
+      jest.useRealTimers();
+    });
+
+    xit("should only run one Job at a time when threadPoolDepth is 1", () => {
+      //
+    });
+
+    xit("should only run two Jobs at a time when threadPoolDepth is 2", () => {
+      //
+    });
+
+    xit("Enqueues Jobs and runs them once the threadPool has capacity", () => {
+      //
+    });
   });
 });
--- src/Kelda.ts
+++ src/Kelda.ts
@@ -1,27 +1,86 @@
 import JobFactory from "./job/JobFactory";
 
+type Resolve = (result?: any) => void;
+type Reject = (error?: Error) => void;
+type EnqueuedJob = [Job, Resolve, Reject];
+
+class Thread {
+  public isIdle: boolean = true;
+
+  public do(job: Job): Promise<any> {
+    this.isIdle = false;
+
+    return job.execute().finally(() => (this.isIdle = true));
+  }
+}
+
+class ThreadPool {
+  private static validate(threadPoolDepth: number) {
+    if (threadPoolDepth <= 0)
+      throw new Error("KeldaError: threadPoolDepth must be greater than 0");
+    // TODO: custom KeldaError class
+  }
+
+  private threads: Thread[];
+  private queue: EnqueuedJob[] = [];
+
+  constructor(threadPoolDepth: number) {
+    ThreadPool.validate(threadPoolDepth);
+
+    this.threads = this.populateThreads(threadPoolDepth);
+  }
+
+  public schedule(job: Job): Promise<any> {
+    const thread = this.getThread();
+
+    if (thread) {
+      return thread.do(job).finally(this.doFromQueue);
+    }
+
+    return new Promise((resolve, reject) => {
+      const toEnqueue: EnqueuedJob = [job, resolve, reject];
+      this.queue.push(toEnqueue);
+    });
+  }
+
+  private doFromQueue() {
+    // if queue
+    // grab a free thread
+    // if free thread
+    // grab oldest queued job
+    // thread.do(job).then(resolve).catch(reject).finally(doFromQueue)
+  }
+
+  private populateThreads(threadPoolDepth: number): Thread[] {
+    return Array(threadPoolDepth)
+      .fill(null)
+      .map(() => new Thread());
+  }
+
+  private getThread(): Thread | undefined {
+    return this.threads.find(thread => thread.isIdle);
+  }
+}
+
 interface KeldaOptions {
   threadPoolDepth: number;
 }
 
 const defaultOptions = {
   threadPoolDepth: 1
-};
+} as KeldaOptions;
 
 class Kelda {
-  private static validateThreadPoolDepth(threadPoolDepth: number) {
-    if (threadPoolDepth <= 0)
-      throw new Error("KeldaError: threadPoolDepth must be greater than 0");
-  }
+  private threadPool: ThreadPool;
 
   constructor({ threadPoolDepth }: KeldaOptions = defaultOptions) {
-    Kelda.validateThreadPoolDepth(threadPoolDepth);
+    this.threadPool = new ThreadPool(threadPoolDepth);
   }
 
   public orderWork(work: Work): Promise<any> {
     const job = JobFactory.getJob(work);
 
-    return job.execute();
+    return this.threadPool.schedule(job);
   }
 }
 
