ef32f8ca89ef892b67498a04ab709abf986ef63e
Rufus Raghunath <rraghun@thoughtworks.com>
2019-12-17 14:58:13 +0100

    Implement skipped Kelda tests

--- src/Kelda.test.ts
+++ src/Kelda.test.ts
@@ -1,20 +1,47 @@
 import Kelda from "./Kelda";
+import MockWorker from "./dom-mocks/MockWorker";
+import MockBlob from "./dom-mocks/MockBlob";
+import MockURL from "./dom-mocks/MockURL";
 
 describe("Kelda", () => {
   const work = () => 1 + 1;
 
   it("can take orders for work when Workers are available", async () => {
+    window.Worker = MockWorker as any;
+    window.Blob = MockBlob as any;
+    window.URL = MockURL as any;
+
     const kelda = new Kelda();
     const result = await kelda.orderWork(work);
 
     expect(result).toBe(2);
   });
 
-  xit("can take orders for work when Workers are unavailable", async () => {
-    // TODO
+  it("can take orders for work when Workers are unavailable", async () => {
+    delete window.Worker;
+    delete window.Blob;
+    delete window.URL;
+
+    const kelda = new Kelda();
+    const result = await kelda.orderWork(work);
+
+    expect(result).toBe(2);
   });
 
-  xit("can take multiple orders and complete them as threads open up", () => {
-    // TODO
+  it("can take multiple orders", async () => {
+    window.Worker = MockWorker as any;
+    window.Blob = MockBlob as any;
+    window.URL = MockURL as any;
+
+    const kelda = new Kelda({ threadPoolDepth: 3 });
+    const workPromises: Promise<any>[] = [];
+
+    Array(10)
+      .fill(null)
+      .forEach(() => kelda.orderWork(work));
+
+    workPromises.forEach(
+      async workPromise => await expect(workPromise).resolves.toBe(2)
+    );
   });
 });
