adef1d4baed26715c3a30ba615f87b89fb8be637
Rufus Raghunath <rraghun@thoughtworks.com>
2019-12-19 12:02:18 +0100

    Add index.d.ts and implement copy-types

---
+++ src/types/index.d.ts
@@ -0,0 +1,15 @@
+declare module "kelda-js" {
+  type Work = () => any;
+
+  interface KeldaOptions {
+    threadPoolDepth: number;
+  }
+
+  class Kelda {
+    constructor(options: KeldaOptions);
+
+    public orderWork(work: Work): Promise<any>;
+  }
+
+  export default Kelda;
+}
--- src/vendor.d.ts
+++
@@ -1,1 +0,0 @@
-// Custom vendor typings go here
